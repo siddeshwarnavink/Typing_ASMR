6c9b54a5b63ca6e0c48a0954210477d80917278c
Siddeshwar <siddeshwar.work@gmail.com>
2024-07-12 13:07:17 +0530

    playing sound on keypress

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/siddeshwarnavink/coding-with-asmr-on
 go 1.22.4
 
 require (
+	github.com/ebitengine/oto/v3 v3.2.0 // indirect
+	github.com/ebitengine/purego v0.7.0 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
 	github.com/robotn/gohook v0.41.0 // indirect
 	github.com/vcaesar/keycode v0.10.1 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"os"
+	"time"
 
+	"github.com/ebitengine/oto/v3"
+	"github.com/hajimehoshi/go-mp3"
 	hook "github.com/robotn/gohook"
 )
+
+func playSound() {
+	fileBytes, err := os.ReadFile("./sounds/keypress-1.mp3")
+	if err != nil {
+		panic("Sound files are missing")
+	}
+
+	fileBytesReader := bytes.NewReader(fileBytes)
+
+	decodedMp3, err := mp3.NewDecoder(fileBytesReader)
+	if err != nil {
+		panic("mp3.NewDecoder failed: " + err.Error())
+	}
+
+	op := &oto.NewContextOptions{}
+	op.SampleRate = 44100
+	op.ChannelCount = 2
+	op.Format = oto.FormatSignedInt16LE
+
+	otoCtx, readyChan, err := oto.NewContext(op)
+	if err != nil {
+		panic("oto.NewContext failed: " + err.Error())
+	}
+
+	<-readyChan
+
+	player := otoCtx.NewPlayer(decodedMp3)
+	player.Play()
+	for player.IsPlaying() {
+		time.Sleep(time.Millisecond)
+	}
+}
 
 func main() {
 	fmt.Println("Coding with ASMR is running. Press 'SHIFT+Q' to stop.")
@@ -14,8 +51,8 @@ func main() {
 		hook.End()
 	})
 
-	hook.Register(hook.KeyDown, []string{"w"}, func(e hook.Event) {
-		fmt.Println("pressed w!!")
+	hook.Register(hook.KeyUp, []string{"A"}, func(e hook.Event) {
+		playSound()
 	})
 
 	s := hook.Start()
